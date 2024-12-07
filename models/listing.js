6f0b94d821c46cb21cbe1d09b4337403c39f2845
him8033 <himanshu8033@gmail.com>
2024-12-07 18:52:03 +0530

    add new model for reviews and also include review Section in listing model Schema

--- models/listing.js
+++ models/listing.js
@@ -14,7 +14,11 @@ const listingSchema = new Schema({
     },
     price: Number,
     location: String,
-    country: String
+    country: String,
+    review: {
+        type: Schema.Types.ObjectId,
+        ref: "Review"
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
