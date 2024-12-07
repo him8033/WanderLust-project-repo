2991a47a756e56d8b39d8264ed046dfff276310f
him8033 <himanshu8033@gmail.com>
2024-12-07 20:27:04 +0530

    review display section add in show.ejs and in their route add populate() method in the query of mongodb

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.post("/listing", validateListing, wrapAsync(async (req, res, next) => {
 
 app.get("/listing/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    let listing = await Listing.findById(id);
+    let listing = await Listing.findById(id).populate("reviews");
     res.render("listing/show.ejs", { listing });
 }))
 
