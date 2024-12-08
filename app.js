060d8e900d82a76a2193ee23c243c78ad7bf47bd
him8033 <himanshu8033@gmail.com>
2024-12-08 12:34:03 +0530

    make a review.js route for seprating all reviews route in a single and seprate file and implement their repuired things

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ const port = 3000;
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const path = require("path");
-const Listing = require("./models/listing.js");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./Schema.js");
-const Review = require("./models/review.js");
 
 const listing = require("./routes/listing.js");
+const review = require("./routes/review.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -36,68 +33,9 @@ app.get("/", (req, res) => {
     res.send("app is working and this is root");
 })
 
-//      ************************        Testing Route
+app.use("/listing", listing);
+app.use("/listing/:id/reviews", review);
 
-// app.get("/testListing",async(req,res) => {
-//     let sampleListing = new Listing({
-//         title: "My new Villa",
-//         description: "By the Beach",
-//         price: 1200,
-//         location: "lucknow, UP",
-//         Country: "India"
-//     })
-
-//     await sampleListing.save();
-//     console.log("sample list is saved");
-//     res.send("Successfully Testing");
-// })
-
-//      Define ServerSide Schema Validation Function 
-
-const validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-}
-
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-}
-
-app.use("/listing",listing);
-
-//      ***********************         Review Post Route
-
-app.post("/listing/:id/reviews", validateReview, wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listing/${listing._id}`);
-}))
-
-//      ************************        Review Delete Route
-
-app.delete("/listing/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listing/${id}`);
-}))
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
--- routes/listing.js
+++ routes/listing.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("../Schema.js");
+const { listingSchema } = require("../Schema.js");
 
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
