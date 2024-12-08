7f5275bcc29b6c084e986ee9a28c531fb605011a
him8033 <himanshu8033@gmail.com>
2024-12-08 21:56:42 +0530

    res.locals method implement with req.flash

--- classroom/server.js
+++ classroom/server.js
@@ -14,15 +14,25 @@ app.use(flash());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+app.use((req,res,next)=>{
+    res.locals.successMsg = req.flash("success");
+    res.locals.errorMsg = req.flash("error");
+    next();
+})
+
 app.get("/register", (req,res)=>{
     let { name= "anonymous" } = req.query;
     req.session.name = name;
-    req.flash("success","user registerd successfully");
+    if(name === "anonymous"){
+        req.flash("error","user not registerd");
+    }else{
+        req.flash("success","user registerd successfully");
+    }
     res.redirect("/hello");
 })
 
 app.get("/hello", (req,res)=>{
-    res.render("page.ejs",{name: req.session.name, msg: req.flash("success")})
+    res.render("page.ejs",{name: req.session.name})
 })
 
 app.get("/rqcount", (req, res) => {
