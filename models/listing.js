78156307edd266701acaf1d934b6218af02b3675
him8033 <himanshu8033@gmail.com>
2024-12-08 10:15:29 +0530

    mongodb middleware .post logic implement for (if delete listing then their reviews also deleted in review tab)

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
     title: {
@@ -10,7 +11,7 @@ const listingSchema = new Schema({
     image: {
         type: String,
         default: "https://plus.unsplash.com/premium_photo-1673138824704-344104aec465?q=80&w=1073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1673138824704-344104aec465?q=80&w=1073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v
+        set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1673138824704-344104aec465?q=80&w=1073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
     },
     price: Number,
     location: String,
@@ -20,6 +21,12 @@ const listingSchema = new Schema({
         ref: "Review"
     }]
 });
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
+    }
+})
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
