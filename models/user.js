ff0348feb4daa0a6f320902efd4995522edc45c6
him8033 <himanshu8033@gmail.com>
2024-12-09 19:28:51 +0530

    make a user.js file in /models for making authication schema with the help of passport-local-mongoose package here we define only email field in shema because passport-local-mongoose package already created username and password(with salting and hashing technique) fields

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema({
+    email: {                    //  here we are only other fields are created because "passport-local-mongoose"
+        type: String,           //  already creates username and password(with salt and hashing) fields 
+        required: true,
+    }
+})
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User",userSchema);
