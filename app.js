072a6d3fa7079c166130f5d87b7a5f677c19e32b
him8033 <himanshu8033@gmail.com>
2024-12-09 20:34:47 +0530

    require (passport, passport-local, ./models/user.js) and implment passport logics in app.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const listing = require("./routes/listing.js");
 const review = require("./routes/review.js");
@@ -48,6 +51,13 @@ app.get("/", (req, res) => {
 
 app.use(session(sessionOption));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next) => {
     res.locals.success = req.flash("success");
