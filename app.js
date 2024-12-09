ea7e035ead0bf16c5acd55b48a8fe13f3cafad51
him8033 <himanshu8033@gmail.com>
2024-12-09 20:59:49 +0530

    create a fake user for testing the passport package in /demouser route

--- app.js
+++ app.js
@@ -65,6 +65,16 @@ app.use((req,res,next) => {
     next();
 })
 
+app.get("/demouser",async (req,res) => {
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "delta-student"
+    })
+
+    let registeredUser = await User.register(fakeUser, "helloworld");
+    res.send(registeredUser);
+})
+
 app.use("/listing", listing);
 app.use("/listing/:id/reviews", review);
 
