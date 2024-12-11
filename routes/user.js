9e6c0f9509033170f750dde574ada9184d1e3a3f
him8033 <himanshu8033@gmail.com>
2024-12-11 18:14:19 +0530

    implement redirect page logic (after login redirect in a same page) using using the help of (req.session and req.locals)

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const wrapAsync = require("../utils/wrapAsync");
 const router = express.Router();
 const User = require("../models/user.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -31,9 +32,10 @@ router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 })
 
-router.post("/login", passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), wrapAsync(async (req, res) => {
+router.post("/login", saveRedirectUrl, passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), wrapAsync(async (req, res) => {
     req.flash("success", "Welcome back to WanderLust!");
-    res.redirect("/listing");
+    let redirectUrl = res.locals.redirectUrl || "/listing";
+    res.redirect(redirectUrl);
 }))
 
 router.get("/logout", (req, res, next) => {
