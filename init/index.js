05da7fe5f4fd00e35171023e05549714729cb8f1
him8033 <himanshu8033@gmail.com>
2024-12-11 20:28:56 +0530

    listing schema updated with user object and add user data in listing with changing data.js file (push the data outside the data.js file i.e. we add user object in index.js file

--- init/index.js
+++ init/index.js
@@ -16,6 +16,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj, owner: "6759a7c59da3fc63fb5a602c"}));
     await Listing.insertMany(initData.data);
     console.log("data was initilized");
 }
