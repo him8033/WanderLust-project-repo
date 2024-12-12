bf946728b2367342061d7ca2a26801703c32a855
him8033 <himanshu8033@gmail.com>
2024-12-12 08:31:19 +0530

    change review schema with author object(for creating review author) and save their author in review.post route and also hide review creation option for logout users:

--- routes/review.js
+++ routes/review.js
@@ -3,14 +3,14 @@ const router = express.Router({ mergeParams: true });
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const Review = require("../models/review.js");
-const { validateReview } = require("../middleware.js");
+const { validateReview, isLoggedIn } = require("../middleware.js");
 
 //     Review Post Route
 
-router.post("/", validateReview, wrapAsync(async (req, res) => {
+router.post("/",isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
-
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
 
     await newReview.save();
