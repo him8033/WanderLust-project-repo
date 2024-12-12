a95c18e37a7283821068814b033e578904e06049
him8033 <himanshu8033@gmail.com>
2024-12-12 20:41:24 +0530

    multer package install for handle (enctype='multipart/form-data') and acquire these package in listing.js file and create a dummy post route for testing multer package

--- routes/listing.js
+++ routes/listing.js
@@ -2,12 +2,18 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listing.js");
+const multer = require("multer");
+const upload = multer({dest: "uploads/"});
 
 router.route("/")
     .get(wrapAsync(listingController.index))
-    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+    // .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+    .post(upload.single("listing[image]"), (req,res) => {
+        console.log(req.body);
+        console.log(req.file);
+        res.send(req.file);
+    })
 
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
