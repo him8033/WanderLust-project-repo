742521f747c08a9aadf1c78e21a21f5e7de91250
him8033 <himanshu8033@gmail.com>
2025-01-28 22:12:06 +0530

    edit.ejs , update route modified for updating image

--- routes/listing.js
+++ routes/listing.js
@@ -9,14 +9,14 @@ const upload = multer({storage});
 
 router.route("/")
     .get(wrapAsync(listingController.index))
-    .post(isLoggedIn, upload.single("listing[image]"), wrapAsync(listingController.createListing));
+    .post(isLoggedIn, upload.single("listing[image]"), validateListing, wrapAsync(listingController.createListing));
     
 
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 router.route("/:id")
     .get(wrapAsync(listingController.showListing))
-    .put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
+    .put(isLoggedIn, isOwner, upload.single("listing[image]"), validateListing, wrapAsync(listingController.updateListing))
     .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
